39f9d84887d7cf1e2c6409a69d9de817f4979f6b
philip <philip.yijie.zhang@gmail.com>
2022-09-09 20:57:58 +0200

    changed to logsumexp and remove lamb in var

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ def get_measures(var_prior, var_likelihood, X_train, Y_train, X_test, Y_test, la
     emp_r_log_risks = []  # empirical gibbs risk
     kls = []  # kl between posterior and prior
     vars_prior_pred = []  # prior predictive variance
+    lambs_optimal = []
     for lamb in lambs:
 
         print(lamb)
@@ -43,70 +44,70 @@ def get_measures(var_prior, var_likelihood, X_train, Y_train, X_test, Y_test, la
         m_N = (beta * S_N @ X_train.T @ Y_train).reshape(-1)  # posterior mean
         p_post = MultivariateNormal(m_N, precision_matrix=S_N_inv)
 
-        # computre posterior predictive distribution, see bishop eq 3.58 and 3.59
-        mean_post_pred = X_test @ m_N
-        cov_post_pred = torch.eye(n_test) / beta + X_test @ S_N @ X_test.T
-        std_post_pred = torch.sqrt(torch.diag(cov_post_pred))
-        p_post_pred = Normal(mean_post_pred, std_post_pred)
+        # compute log_p
+        samples_post = p_post.sample((n_post_samples,))
+        log_p_test = Normal(
+            X_test @ samples_post.T, torch.sqrt(var_likelihood)
+        ).log_prob(Y_test)
+        log_p_train = Normal(
+            X_train @ samples_post.T, torch.sqrt(var_likelihood)
+        ).log_prob(Y_train)
 
         # get bayesian generalization loss
-        log_risk = -p_post_pred.log_prob(Y_test).mean().item()
-        # log_risk1 = (
-        #     -torch.log(
-        #         torch.exp(
-        #             Normal(
-        #                 X_test @ samples_post.T, torch.sqrt(var_likelihood / lamb)
-        #             ).log_prob(Y_test)
-        #         ).mean(1)
-        #     )
-        #     .mean()
-        #     .item()
-        # )
+        log_risk = (
+            torch.log(torch.tensor(n_post_samples)) - torch.logsumexp(log_p_test, 1)
+        ).mean()
         log_risks.append(log_risk)
 
         # get expected gibbs loss
-        samples_post = p_post.sample((n_post_samples,))
-        r_log_risk = (
-            -Normal(X_test @ samples_post.T, torch.sqrt(var_likelihood / lamb))
-            .log_prob(Y_test)
-            .mean()
-            .item()
-        )
+        r_log_risk = -log_p_test.mean().item()
         r_log_risks.append(r_log_risk)
 
         # get empirical gibbs loss
-        emp_r_log_risk = (
-            -Normal(X_train @ samples_post.T, torch.sqrt(var_likelihood / lamb))
-            .log_prob(Y_train)
-            .mean()
-            .item()
-        )
+        emp_r_log_risk = -log_p_train.mean().item()
         emp_r_log_risks.append(emp_r_log_risk)
 
         # prior predictive variance
         p_prior = MultivariateNormal(torch.zeros(d_x), torch.eye(d_x) * var_prior)
         samples_prior = p_prior.sample((n_prior_samples,))
+        n_D_n = n_test / n_train  # number of D_n sampled
         samples_X_n = torch.tensor(
-            np.asarray(np.vsplit(X_test.numpy(), n_test / n_train))
-        )
+            np.asarray(np.vsplit(X_test.numpy(), n_D_n))
+        )  # n_D_n x n_train x d_x
         samples_Y_n = torch.tensor(
-            np.asarray(np.vsplit(Y_test.numpy(), n_test / n_train))
-        )
+            np.asarray(np.vsplit(Y_test.numpy(), n_D_n))
+        )  # n_D_n x n_train x 1
+        log_p_prior = Normal(
+            samples_X_n @ samples_prior.T, torch.sqrt(var_likelihood)
+        ).log_prob(
+            samples_Y_n
+        )  # n_D_n x n_train x n_prior_samples
         var_prior_pred = torch.mean(
             torch.var(
                 torch.sum(
-                    Normal(
-                        samples_X_n @ samples_prior.T, torch.sqrt(var_likelihood)
-                    ).log_prob(samples_Y_n),
+                    log_p_prior,
                     1,
                 ),
-                1,
+                0,
             )
         ).item()
         vars_prior_pred.append(var_prior_pred)
 
-        # kl
+        # kl between p^lamb and prior
         kl = torch.distributions.kl.kl_divergence(p_post, p_prior).item()
         kls.append(kl)
 
-    return log_risks, r_log_risks, emp_r_log_risks, kls, vars_prior_pred
+        # compute lamb^*
+        lamb_optimal = torch.sqrt(
+            2 * (kl + torch.log(torch.tensor(20.0))) / var_prior_pred
+        )
+        lambs_optimal.append(lamb_optimal.item())
+
+    return (
+        log_risks,
+        r_log_risks,
+        emp_r_log_risks,
+        kls,
+        vars_prior_pred,
+        lambs_optimal,
+    )
